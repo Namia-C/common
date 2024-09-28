eca5cf6848d8832457f3c8b3c46b2c9dec7a881f
OpenWrt-X <mk258855@163.com>
2024-09-28 19:25:33 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -319,9 +319,12 @@ mv -f uniq.conf feeds.conf.default
 
 # 这里增加了源,要对应的删除/etc/opkg/distfeeds.conf插件源
 cat >>"feeds.conf.default" <<-EOF
+
 src-git danshui1 https://github.com/Namia-Q/openwrt-package.git;${SOURCE}
 src-git helloworld https://github.com/fw876/helloworld.git
 src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages;main
+src-git small https://github.com/kenzok8/small;master
+
 EOF
 ./scripts/feeds update -a
 
